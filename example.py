3b51492dcbcc7162da70b0a9d8ffb6b0534f589d
Canadew <110163919+chun-awa@users.noreply.github.com>
2025-01-03 06:14:53 +0800

    Update example.py

--- example.py
+++ example.py
@@ -14,7 +14,6 @@ button2=LingmoPyUI.LingmoSlider(window)
 button2.move(0,a)
 a+=40
 button2=LingmoPyUI.LingmoIconButton(LingmoIconDef.Accept,window,content='1111')
-button2.setDisplay(LingmoPyUI.LingmoIconButton.TextBesideIcon)
 button2.move(0,a)
 a+=40
 window.resize(1000,1000)
@@ -24,4 +23,4 @@ scrollbar=LingmoPyUI.LingmoScrollBar(frame,target=window)
 scrollbar.orientation=LingmoPyUI.Qt.Orientation.Horizontal
 tooltip=LingmoPyUI.LingmoToolTip(button,content='发送到路口收到了科技焚枯食淡看来是道具发送到路口收到了科技焚枯食淡看来是道具发送到路口收到了科技焚枯食淡看来是道具')
 if __name__ == "__main__":
-    LingmoApp.run()
+    LingmoPyUI.LingmoApp.run()
