39408cac722966be63c99064971cd249899e2d07
chun-awa <110163919+chun-awa@users.noreply.github.com>
2025-01-18 10:51:43 +0800

    style: reformat all files using black

--- LingmoPyUI/LingmoApp.py
+++ LingmoPyUI/LingmoApp.py
@@ -7,28 +7,29 @@ import sys
 from . import LingmoIconDef
 
 packagePath = os.path.dirname(os.path.abspath(__file__))
-appid='com.lingmo.pyui.1.0'
-windowIcon=packagePath+'/Image/icon.png'
-locale=QLocale()
-launcher=QObject()
-useSystemAppBar=False
-_app=QApplication([])
-_translator=QTranslator(QApplication.instance())
+appid = "com.lingmo.pyui.1.0"
+windowIcon = packagePath + "/Image/icon.png"
+locale = QLocale()
+launcher = QObject()
+useSystemAppBar = False
+_app = QApplication([])
+_translator = QTranslator(QApplication.instance())
 QApplication.installTranslator(_translator)
-uiLanguages=locale.uiLanguages()
+uiLanguages = locale.uiLanguages()
 for i in uiLanguages:
-    basename='lingmoui_'+QLocale(i).name()
-    if _translator.load(os.path.dirname(sys.argv[0])+basename):
+    basename = "lingmoui_" + QLocale(i).name()
+    if _translator.load(os.path.dirname(sys.argv[0]) + basename):
         _app.translate()
         break
+
 
 def run():
     _app.exec()
 
+
 def iconData(keyword):
-    arr=QJsonArray()
+    arr = QJsonArray()
     for i in LingmoIconDef.__dict__:
-        if (keyword in i) or keyword=='':
-            value=QJsonValue({'name':i,'icon':LingmoIconDef.__dict__[i]})
+        if (keyword in i) or keyword == "":
+            value = QJsonValue({"name": i, "icon": LingmoIconDef.__dict__[i]})
             arr.append(value)
-            
--- LingmoPyUI/LingmoUnits.py
+++ LingmoPyUI/LingmoUnits.py
@@ -4,31 +4,36 @@ from PySide6.QtGui import *
 
 from . import LingmoTextStyle
 
+
 class iconSizes:
-    small=16
-    smallMedium=22
-    medium=32
-    large=48
-    huge=64
-    enormous=128
-rowHeight=iconSizes.large*0.95
-rowHeightAlt=rowHeight*0.8
-fontMertics=QFontMetrics(LingmoTextStyle._family)
-gridUnit=fontMertics.height()
-extendBorderWidth=0
-smallSpacing=6
-largeSpacing=smallSpacing*2
-devicePixelRatio=QApplication.primaryScreen().devicePixelRatio()
+    small = 16
+    smallMedium = 22
+    medium = 32
+    large = 48
+    huge = 64
+    enormous = 128
+
+
+rowHeight = iconSizes.large * 0.95
+rowHeightAlt = rowHeight * 0.8
+fontMertics = QFontMetrics(LingmoTextStyle._family)
+gridUnit = fontMertics.height()
+extendBorderWidth = 0
+smallSpacing = 6
+largeSpacing = smallSpacing * 2
+devicePixelRatio = QApplication.primaryScreen().devicePixelRatio()
+
+
 def roundIconSize(size):
-    if size<16:
+    if size < 16:
         return size
-    elif size<22:
+    elif size < 22:
         return 16
-    elif size<32:
+    elif size < 32:
         return 22
-    elif size<48:
+    elif size < 48:
         return 32
-    elif size<64:
+    elif size < 64:
         return 48
     else:
         return size
